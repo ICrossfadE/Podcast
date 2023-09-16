faa8bac0b4fbf43f744d3932777fd1800eba4a08
Maks <Crossfade143@gmail.com>
2023-09-16 23:59:33 +0300

    create adaptive header-nav

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const menuButton = document.querySelector("#header-menu");
+const menu = document.querySelector(".header-nav");
+
+menuButton.onclick = () => {
+  menu.classList.toggle("open");
+};
