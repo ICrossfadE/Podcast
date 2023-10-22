db7b5c69d61a72bfb4e8fda8f977e669500dc118
Maks <Crossfade143@gmail.com>
2023-10-22 22:06:37 +0300

    create state

--- src/js/app.js
+++ src/js/app.js
@@ -3,27 +3,23 @@ import data from "../js/data/podcast-list.json";
 import { showNavbar } from "./modules/showNavbar.js";
 import { loadAudio } from "./modules/loadAudio.js";
 import { loadIntroAudio } from "./modules/loadIntroAudio.js";
-import { playPauseAudio } from "./modules/playAudio.js";
-import { playPauseAudioIntro } from "./modules/playAudioIntro.js";
+import { playPauseAudio } from "./modules/playPauseAudio.js";
+import { playPauseAudioIntro } from "./modules/playPauseAudioIntro.js";
 import { switchAudio } from "./modules/switchAudio.js";
 import { updateProgress } from "./modules/updateProgress.js";
 import { setVolume } from "./modules/setVolume.js";
+import { state } from "./modules/state.js";
 
 flsFunctions.isWebp();
 
 showNavbar();
 
-let audioIndex = 9;
-let isPlaying = false;
-let isPlayingIntro = false;
-// let isRandom = false;
-
 window.addEventListener("load", () => {
   loadIntroAudio();
-  loadAudio(data[audioIndex], setVolume);
+  loadAudio(data[state.audioIndex], setVolume);
 });
 
-playPauseAudio(isPlaying);
-playPauseAudioIntro(isPlayingIntro);
-switchAudio(audioIndex, loadAudio, data);
+playPauseAudio(state.isPlaying);
+playPauseAudioIntro(state.isPlayingIntro);
+switchAudio(state.audioIndex, loadAudio, data);
 updateProgress();
--- src/js/modules/isWebp.js
+++ src/js/modules/isWebp.js
@@ -7,8 +7,11 @@ export function isWebp() {
     webP.src =
       "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
   }
+
   testWebP(function (support) {
     let className = support == true ? "webp" : "no-webp";
     document.documentElement.classList.add(className);
   });
+
+  console.log("ok");
 }
